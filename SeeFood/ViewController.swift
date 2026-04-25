4721f4279e637084c535cd2bfb88716204aabf29
Ari Agafonova <agafonova_arina@icloud.com>
2026-04-25 14:56:07 +0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Арина Агафонова on 25.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
